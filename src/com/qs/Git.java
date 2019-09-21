da493bd50620fd730d60a2a8cfd3feb6f8d446a3
qcy <1244976837@qq.com>
2019-09-21 19:40:14 +0800

    first project

---
+++ src/com/qs/Git.java
@@ -0,0 +1,18 @@
+package com.qs;
+
+
+
+public class Git {
+
+    public static void main(String[] args) {
+        System.out.println(1111);
+        System.out.println(22222);
+        System.out.println(333333);
+        System.out.println(444444444);
+
+
+
+    }
+
+
+}
